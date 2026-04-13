18c06d4e25f5878138da35194f3d2593246369d9
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-13 13:55:50 -0700

    fixed js links

--- global.js
+++ global.js
@@ -5,18 +5,24 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-  { url: '/dsc_106_portfolio`', title: 'Home' },
-  { url: '/dsc_106_portfolio/projects/', title: 'Projects' },
-  { url: '/dsc_106_portfolio/contact/', title: 'Contact' },
+  { url: '/', title: 'Home' },
+  { url: '/projects/', title: 'Projects' },
+  { url: '/contact/', title: 'Contact' },
   { url: 'https://github.com/Lyn-Mansfield', title: 'GitHub' },
 ];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
 for (let p of pages) {
   let url = p.url;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
   let title = p.title;
+  
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
 
