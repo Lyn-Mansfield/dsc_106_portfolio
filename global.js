d974bc45fb330f49afa5fa80113e210de6fa48ee
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-13 13:47:17 -0700

    tried to automate nav menu

--- global.js
+++ global.js
@@ -3,3 +3,26 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+
+if (currentLink !== undefined) {
+  currentLink.classList.add('current');
+}
