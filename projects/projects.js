bb10969a68aea5136a037da81db4f7dc8b50dc9e
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-21 17:50:50 -0700

    added log to see what's up with projects not passing JSON check

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
+console.log(projects)
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
