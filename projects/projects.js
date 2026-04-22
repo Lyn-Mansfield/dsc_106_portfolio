bbd3a2fe5706e6ec60e2602395b8e895aeb3605e
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-21 18:48:07 -0700

    fixed innerText typo

--- projects/projects.js
+++ projects/projects.js
@@ -4,7 +4,7 @@ const projects = await fetchJSON('../lib/projects.json');
 // Update title
 const projectnum = await projects.length;
 const projectTitle = document.querySelector('#project-title');
-projectTitle.innertext = `${projectnum} Projects`;
+projectTitle.innerText = `${projectnum} Projects`;
 // Fill page with actual projects
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
