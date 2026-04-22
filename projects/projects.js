4d0db71909562958f8b798525b70605fff17d53e
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-21 18:35:32 -0700

    should now properly update projects title

--- projects/projects.js
+++ projects/projects.js
@@ -3,8 +3,8 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 // Update title
 const projectnum = await projects.length;
-const projectTitle = document.querySelector('.project-title');
-projectTitle.innerText(`${projectnum} Projects`);
+const projectTitle = document.querySelector('#project-title');
+projectTitle.innertext = `${projectnum} Projects`;
 // Fill page with actual projects
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
