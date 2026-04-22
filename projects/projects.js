031211f962f04c35f5d7dd15889765258ba64972
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-21 17:52:51 -0700

    added more testing

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-console.log(projects)
+console.log(projects, typeof projects);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
