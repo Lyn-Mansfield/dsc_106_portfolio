0f32d701f33be2aa383a86ab3ac4d6b57c5ac294
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-27 15:23:36 -0700

    fixed pie drawing code

--- projects/projects.js
+++ projects/projects.js
@@ -9,17 +9,23 @@ let arc = arcGenerator({
 });
 d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
+// Calculate total length of data
 let data = [1, 2];
-
 let total = 0;
+for (let d of data) {
+  total += d;
+}
+
 let angle = 0;
 let arcData = [];
+// Define each arc piece
 for (let d of data) {
     total += d;
     let endAngle = angle + (d / total) * 2 * Math.PI;
     arcData.push({ startAngle: angle, endAngle });
     angle = endAngle;
 }
+// Draw each arc piece to make a whole pie chart
 let arcs = arcData.map((d) => arcGenerator(d));
 arcs.forEach((arc) => {
     d3.select('svg').append('path').attr('d', arc).attr('fill', () => d3.scaleOrdinal(d3.schemeCategory10)(Math.random()));
