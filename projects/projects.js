687f68cdedbd8125cdd7f5cf72a581f653f22c82
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-27 15:26:24 -0700

    use d3.pie() instead of reinventing the wheel, badum-tss

--- projects/projects.js
+++ projects/projects.js
@@ -2,33 +2,18 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // Pie Chart Block
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-let arc = arcGenerator({
-    startAngle: 0,
-    endAngle: 2 * Math.PI,
-});
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 
-// Calculate total length of data
-let data = [1, 2];
-let total = 0;
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
 // Define each arc piece
-for (let d of data) {
-    total += d;
-    let endAngle = angle + (d / total) * 2 * Math.PI;
-    arcData.push({ startAngle: angle, endAngle });
-    angle = endAngle;
-}
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 // Draw each arc piece to make a whole pie chart
 let arcs = arcData.map((d) => arcGenerator(d));
 arcs.forEach((arc) => {
-    d3.select('svg').append('path').attr('d', arc).attr('fill', () => d3.scaleOrdinal(d3.schemeCategory10)(Math.random()));
+    d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', () => d3.scaleOrdinal(d3.schemeCategory10)(Math.random()));
 });
 
 // Import Projects Data Block
