3e8179dfe75e0c60e703acc19e0c0aef4c37f946
Lyn Mansfield <imansfield@ucsd.edu>
2026-04-29 10:59:43 -0700

    added search bar

--- projects/projects.js
+++ projects/projects.js
@@ -3,16 +3,17 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // Pie Chart Block
 
-// Define each arc piece
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let projects = fetchJSON('../lib/projects.json');
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
+// Define each arc piece
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
@@ -36,6 +37,21 @@ data.forEach((d, i) => {
   .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
 console.log("Successfully rendered projects pie chart legend!")
+
+let query = '';
+let searchInput = document.querySelector('.searchBar');
+
+searchInput.addEventListener('change', (event) => {
+  // update query value
+  query = event.target.value;
+  // filter projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  // render filtered projects
+  renderProjects(filteredProjects, projectsContainer, 'h2');
+});
 
 // Import Projects Data Block
 const projects = await fetchJSON('../lib/projects.json');
