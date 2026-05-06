495efc56552648e3a7a88ac0dfc1fdb0b4f500ec
Lyn Mansfield <imansfield@ucsd.edu>
2026-05-06 15:00:08 -0700

    added tooltip functionality, and made dots bigger based on commit size

--- meta/main.js
+++ meta/main.js
@@ -1,45 +1,44 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 async function loadData() {
-  const data = await d3.csv('loc.csv', (row) => ({
-    ...row,
-    line: Number(row.line), // or just +row.line
-    depth: Number(row.depth),
-    length: Number(row.length),
-    date: new Date(row.date + 'T00:00' + row.timezone),
-    datetime: new Date(row.datetime),
-  }));
+    const data = await d3.csv('loc.csv', (row) => ({
+        ...row,
+        line: Number(row.line), // or just +row.line
+        depth: Number(row.depth),
+        length: Number(row.length),
+        date: new Date(row.date + 'T00:00' + row.timezone),
+        datetime: new Date(row.datetime),
+    }));
 
-  return data;
+    return data;
 }
 
 function processCommits(data) {
-  return d3
-    .groups(data, (d) => d.commit)
-    .map(([commit, lines]) => {
-      let first = lines[0];
-      let { author, date, time, timezone, datetime } = first;
-      let ret = {
-        id: commit,
-        url: 'https://github.com/Lyn-Mansfield/dsc_106_portfolio/commit/' + commit,
-        author,
-        date,
-        time,
-        timezone,
-        datetime,
-        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-        totalLines: lines.length,
-      };
+    return d3.groups(data, (d) => d.commit)
+        .map(([commit, lines]) => {
+            let first = lines[0];
+            let { author, date, time, timezone, datetime } = first;
+            let ret = {
+                id: commit,
+                url: 'https://github.com/Lyn-Mansfield/dsc_106_portfolio/commit/' + commit,
+                author,
+                date,
+                time,
+                timezone,
+                datetime,
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                totalLines: lines.length,
+            };
 
-      Object.defineProperty(ret, 'lines', {
-        value: lines,
-        configurable: false,
-        writable: false,
-        enumerable: false,
-      });
+            Object.defineProperty(ret, 'lines', {
+                value: lines,
+                configurable: false,
+                writable: false,
+                enumerable: false,
+            });
 
-      return ret;
-    });
+            return ret;
+        });
 }
 function renderCommitInfo(data, commits) {
     // Create the dl element
@@ -93,7 +92,7 @@ function renderScatterPlot(data, commits) {
         .append('svg')
         .attr('viewBox', `0 0 ${width} ${height}`)
         .style('overflow', 'visible');
-    
+
     const xScale = d3
         .scaleTime()
         .domain(d3.extent(commits, (d) => d.datetime))
@@ -101,20 +100,34 @@ function renderScatterPlot(data, commits) {
         .nice();
     const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
 
+    // Preprocessing step
+    const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+    const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+    const rScale = d3
+        .scaleSqrt()
+        .domain([minLines, maxLines])
+        .range([3, 21]);
+    
     const dots = svg.append('g').attr('class', 'dots');
     dots.selectAll('circle')
-        .data(commits)
+        .data(sortedCommits)
         .join('circle')
         .attr('cx', (d) => xScale(d.datetime))
         .attr('cy', (d) => yScale(d.hourFrac))
-        .attr('r', 5)
+        .attr('r', (d) => rScale(d.totalLines))
         .attr('fill', 'steelblue')
+        .style('fill-opacity', 0.7) // Add transparency for overlapping dots
         .on('mouseenter', (event, commit) => {
+            d3.select(event.currentTarget).style('fill-opacity', 1); // Full opacity on hover
             renderTooltipContent(commit);
+            updateTooltipVisibility(true);
+            updateTooltipPosition(event);
         })
-        .on('mouseleave', () => {
-            // TODO: Hide the tooltip
+        .on('mouseleave', (event) => {
+            d3.select(event.currentTarget).style('fill-opacity', 0.7);
+            updateTooltipVisibility(false);
         });
+
 
     const margin = { top: 10, right: 10, bottom: 30, left: 20 };
     const usableArea = {
@@ -165,7 +178,14 @@ function renderTooltipContent(commit) {
 }
 
 function updateTooltipVisibility(isVisible) {
-  const tooltip = document.getElementById('commit-tooltip');
-  tooltip.hidden = !isVisible;
+    const tooltip = document.getElementById('commit-tooltip');
+    tooltip.hidden = !isVisible;
+}
+
+function updateTooltipPosition(event) {
+    const tooltip = document.getElementById('commit-tooltip');
+    const xOffset = 10;
+    tooltip.style.left = `${event.clientX + xOffset}px`;
+    tooltip.style.top = `${event.clientY}px`;
 }
 
